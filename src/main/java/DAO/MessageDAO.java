270134c95a61f9beaba6b33fd3cd702f81f2a378
Oscar Ramirez <oramirezent@gmail.com>
2024-04-03 12:48:55 +0000

    Message getAll/getByID

--- src/main/java/DAO/MessageDAO.java
+++ src/main/java/DAO/MessageDAO.java
@@ -1,10 +1,11 @@
 package DAO;
 
 import Model.Message;
+import Util.ConnectionUtil;
 
 import java.util.List;
 import java.util.ArrayList;
-import Util.ConnectionUtil;
+import java.sql.*;
 
 
 public class MessageDAO {
@@ -20,21 +21,58 @@ public class MessageDAO {
 
 
     /* 
-     * Get all messages
      * @TODO: retrieve all messages from the Message table
      * @return all messages
      */
-    public List<Message> getAllMessages() {
-        return null;
+    public List<Message> retrieveAllMessages() {
+        Connection connection = ConnectionUtil.getConnection();
+        List<Message> messages = new ArrayList<>();
+        
+        try {
+            String sql = "select * from message;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                Message message = new Message(
+                    rs.getInt("message_id"), 
+                    rs.getInt("posted_by"),
+                    rs.getString("message_text"),
+                    rs.getLong("time_posted_epoch")
+                    );
+                messages.add(message);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        return messages;
     }
 
 
     /* 
-     * @TODO: Get message by Id
+     * @TODO: Get message by ID
      * @return a message
      */
-    public Message getMessage(int messageId) {
-        return null;
+    public Message retrieveMessage(int messageId) {
+        Connection connection = ConnectionUtil.getConnection();
+        Message messageToReturn = new Message();
+
+        try {
+            String sql = "select * from message where message_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, messageId);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while(rs.next()) {
+                messageToReturn.setMessage_id(rs.getInt("message_id"));
+                messageToReturn.setPosted_by(rs.getInt("posted_by"));
+                messageToReturn.setMessage_text(rs.getString("message_text"));
+                messageToReturn.setTime_posted_epoch(rs.getLong("time_posted_epoch"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return messageToReturn;
     }
 
     
